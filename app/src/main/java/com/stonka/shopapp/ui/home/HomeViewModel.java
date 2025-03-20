9363facfcae63fba26fd5aa88efc08ce79fea085
GT <98935@g.elearn.uz.zgora.pl>
2025-03-20 19:30:33 +0100

    products

--- app/src/main/java/com/stonka/shopapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/stonka/shopapp/ui/home/HomeViewModel.java
@@ -3,17 +3,34 @@ package com.stonka.shopapp.ui.home;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.stonka.shopapp.R;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<Product>> productList;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        productList = new MutableLiveData<>();
+        loadProducts();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    private void loadProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Laptop", 3499.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Telfon", 1599.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Słuchawki", 199.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Laptop", 3499.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Telfon", 1599.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Słuchawki", 199.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Laptop", 3499.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Telfon", 1599.99, R.drawable.ic_notifications_black_24dp));
+        products.add(new Product("Słuchawki", 199.99, R.drawable.ic_notifications_black_24dp));
+        productList.setValue(products);
+    }
+
+    public LiveData<List<Product>> getProductList() {
+        return productList;
     }
 }
