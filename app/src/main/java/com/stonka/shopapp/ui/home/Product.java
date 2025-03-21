9700396609831cd4ac0f4baa9b862a13e09e6db1
P1200 <sekwoja5@gmail.com>
2025-03-21 11:50:38 +0100

    Replace static products data with downloading them from database.

--- app/src/main/java/com/stonka/shopapp/ui/home/Product.java
+++ app/src/main/java/com/stonka/shopapp/ui/home/Product.java
@@ -3,9 +3,13 @@ package com.stonka.shopapp.ui.home;
 public class Product {
     private String name;
     private double price;
-    private int imageResource;
+    private String imageResource;
 
-    public Product(String name, double price, int imageResource) {
+    public Product() {
+
+    }
+
+    public Product(String name, double price, String imageResource) {
         this.name = name;
         this.price = price;
         this.imageResource = imageResource;
@@ -19,7 +23,19 @@ public class Product {
         return price;
     }
 
-    public int getImageResource() {
+    public String getImageResource() {
         return imageResource;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setImageResource(String imageResource) {
+        this.imageResource = imageResource;
     }
 }
