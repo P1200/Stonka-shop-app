86deb8708c63a1c90bd720bbda6a3a8f201daf8c
P1200 <sekwoja5@gmail.com>
2025-04-15 21:26:13 +0200

    Remove useless notifications tab.

--- app/src/main/java/com/stonka/shopapp/MainActivity.java
+++ app/src/main/java/com/stonka/shopapp/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_shakeomat, R.id.navigation_notifications, R.id.navigation_account)
+                R.id.navigation_home, R.id.navigation_shakeomat, R.id.navigation_account)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
